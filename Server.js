56569ae0761f95f03b8380f1720f959931bd1f02
Manas R <122677792+1deringmanas1331@users.noreply.github.com>
2023-12-18 15:21:41 +0530

    Server.js

---
+++ Server.js
@@ -0,0 +1,21 @@
+
+const http = require('http');
+
+
+function generateRandomPort() {
+  return Math.floor(Math.random() * (9100 - 2500 + 1)) + 2500;
+}
+
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello, this is a random server!\n');
+});
+
+
+const port = generateRandomPort();
+
+
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
